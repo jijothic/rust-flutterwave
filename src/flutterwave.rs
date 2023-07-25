74982e57daa742cb6d7c000edab36ce49e423c94
jijothic <george.jijothic@gmail.com>
2023-07-26 02:20:14 +0300

    still some house keeping :,(

--- src/flutterwave.rs
+++ src/flutterwave.rs
@@ -1,49 +1,55 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value; 
 
 #[derive(Debug, Serialize)]
-struct FlutterwaveChargeRequest<'a> {
-    tx_ref: &'a str,
-    amount: &'a str,
-    currency: &'a str,
-    redirect_url: &'a str,
-    payment_type: &'a str,
+pub struct FlutterwaveChargeRequest<'a> {
+    pub tx_ref: &'a str,
+    pub amount: &'a str,
+    pub currency: &'a str,
+    pub redirect_url: &'a str,
+    pub payment_options: Option<Value>, // Use serde_json::Value for handling dynamic JSON fields
+    pub meta: Option<Value>,
+    pub customer: Option<Value>,
+    pub customizations: Option<Value>,
+    pub subaccounts: Option<Value>,
+    pub payment_plan: Option<Value>,
 }
 
 #[derive(Debug, Deserialize)]
-struct FlutterwaveChargeResponse {
-    status: String,
-    message: String,
-    data: FlutterwaveChargeData,
+pub struct FlutterwaveChargeResponse {
+    pub status: String,
+    pub message: String,
+    pub data: FlutterwaveChargeData,
 }
 
 #[derive(Debug, Deserialize)]
-struct FlutterwaveChargeData {
-    link: String,
+pub struct FlutterwaveChargeData {
+    pub link: String,
 }
 
 #[derive(Debug, Serialize)]
-struct FlutterwaveVerifyRequest<'a> {
-    tx_ref: &'a str,
-    otp: &'a str,
+pub struct FlutterwaveVerifyRequest<'a> {
+    pub tx_ref: &'a str,
+    pub otp: &'a str,
 }
 
 #[derive(Debug, Deserialize)]
-struct FlutterwaveVerifyResponse {
-    status: String,
-    message: String,
-    data: FlutterwaveVerifyData,
+pub struct FlutterwaveVerifyResponse {
+    pub status: String,
+    pub message: String,
+    pub data: FlutterwaveVerifyData,
 }
 
 #[derive(Debug, Deserialize)]
-struct FlutterwaveVerifyData {
-    tx_ref: String,
-    flw_ref: String,
-    amount: String,
-    currency: String,
-    charged_amount: String,
+pub struct FlutterwaveVerifyData {
+    pub tx_ref: String,
+    pub flw_ref: String,
+    pub amount: String,
+    pub currency: String,
+    pub charged_amount: String,
 }
 
-async fn  create_flutterwave_charge(
+pub async fn create_flutterwave_charge(
     api_key: &str,
     secret_key: &str,
     charge_request: FlutterwaveChargeRequest<'_>,
@@ -64,7 +70,7 @@ async fn  create_flutterwave_charge(
     Ok(res)
 }
 
-async fn verify_flutterwave_charge(
+pub async fn verify_flutterwave_charge(
     api_key: &str,
     secret_key: &str,
     verify_request: FlutterwaveVerifyRequest<'_>,
@@ -86,6 +92,26 @@ async fn verify_flutterwave_charge(
         .await?;
 
     Ok(res)
+    //TODO
+    //let charge_response = create_flutterwave_charge(api_key, secret_key, charge_request).await?;
+//println!("Charge response: {:?}", charge_response);
+
+// Check if the charge was successful before proceeding with verification
+//if charge_response.status == "success" {
+    // In a real-world scenario, you would redirect the user to the URL returned in `charge_response.data.link`
+    // for them to make the payment, and then use the following code to verify the payment
+    // after the user has completed the payment process and returned to your website.
+    //let verify_request = FlutterwaveVerifyRequest {
+        //tx_ref,
+        //otp: "12345",
+   // };
+    //let verify_response = verify_flutterwave_charge(api_key, secret_key, verify_request).await?;
+   // println!("Verify response: {:?}", verify_response);
+//} else {
+    // Handle the failed charge here (e.g., display an error message to the user)
+    //println!("Charge failed: {:?}", charge_response.message);
+//}
+
 }
 
 #[tokio::main]
@@ -99,11 +125,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let payment_type = "card";
 
     let charge_request = FlutterwaveChargeRequest {
+        
         tx_ref,
         amount,
         currency,
         redirect_url,
-        payment_type,
+        payment_options, // Use serde_json::Value for handling dynamic JSON fields
+        meta,
+        customer,
+        customizations,
+        subaccounts,
+        payment_plan
     };
 
     let charge_response = create_flutterwave_charge(api_key, secret_key, charge_request).await?;
